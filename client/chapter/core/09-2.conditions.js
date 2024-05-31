49a2fa76d7e283b01d1c4a7ea3d16ba704d48cce
siyo0723 <junior0723@naver.com>
2024-05-31 16:15:14 +0900

    0531 수업진도 if문까지

--- client/chapter/core/09-2.conditions.js
+++ client/chapter/core/09-2.conditions.js
@@ -6,56 +6,48 @@ let a = 10;
 let b = '';
 let value = Boolean(b);
 
-// 논리곱(그리고) 연산자 F&& 
+// 논리곱(그리고) 연산자 F&&
 let AandB = a && b;
-console.log('AandB : ',AandB)
-
+console.log('AandB : ', AandB);
 
 // 논리곱 할당 연산자 Logical AND Assignment
 // a &&= b
 
-
-
 // 논리합(또는) 연산자 ||
 let AorB = a || b;
-console.log('AorB : ',AorB)
-
+console.log('AorB : ', AorB);
 
 // 논리합 할당 연산자 Logical OR Assignment
 // a ||= b
 
-
-
 // 부정 연산자
 let reverseValue = !value;
-console.log('reverseValue : ',reverseValue)
-
+console.log('reverseValue : ', reverseValue);
 
 // 조건 처리
 
 // 첫번째 Falsy를 찾는 연산 (&&)
-let whichFalsy = true && ' ' && [] && {thisIsFalsy:false};
-
-
-
-
-
+let whichFalsy = true && ' ' && [] && { thisIsFalsy: false };
 
 // 첫번째 Truthy를 찾는 연산 (||)
-let whichTruthy = false || '' || [2,3].length || {thisIsTruthy:true};
+let whichTruthy = false || '' || [2, 3].length || { thisIsTruthy: true };
 
+console.clear();
 
+let userName = prompt('누구세요?');
 
+if (userName?.toLowerCase() === 'admin') {
+  let password = prompt('비밀번호는?');
 
-
-
-
-
-
-
-
-
-
-
-
-
+  if (password?.toLowerCase() === 'themaster') {
+    console.log('환영합니다~~!! 짝짝');
+  } else if (password === null) {
+    console.log('취소!');
+  } else {
+    console.log('비밀번호를 잘못 입력하셨습니다.');
+  }
+} else if (userName === null || userName?.replace(/\s*/g, '') === '') {
+  console.log('취소!');
+} else {
+  console.log('실패!');
+}
