798c0707a3f4ba6d7bb002f724160b647de2fdee
siyo0723 <junior0723@naver.com>
2024-06-27 00:05:48 +0900

    05.randomDice 재설정

--- client/lib/dom/endScroll.js
+++ client/lib/dom/endScroll.js
@@ -3,7 +3,6 @@ import { isString } from '../utils/type.js'
 import { getNode } from './getNode.js'
 
 
-
 export function endScroll(node){
   if(isString(node)) node = getNode(node);
   return node.scrollTop = node.scrollHeight
